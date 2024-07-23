a9c60a5db36344e79335ecbc924d160e1f7821b4
IcedFire34 <omar.faruk.bozkaya@gmail.com>
2024-07-23 15:06:34 +0300

    index sayfası ve kurumsal sayfası tamamlandı

---
+++ pentas/pentas/pentas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pentas.MvcApplication" Language="C#" %>
---
+++ pentas/pentas/pentas/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir derleme hakkındaki genel Bilgiler aşağıdakiler üzerinden denetlenir
+// denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+// öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("pentas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pentas")]
+[assembly: AssemblyCopyright("Telif hakkı ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özelliğinin false olarak ayarlanması bu derlemedeki türleri COM bileşenlerine
+//COM görünmez kılar.  Bu derlemedeki bir türe şu kaynaktan erişmeniz gerekiyorsa:
+// COM, bu tür üzerinde ComVisible özniteliğini true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'a açılmışsa aşağıdaki GUID typelib'in ID'si içindir
+[assembly: Guid("d4624173-d79a-41a9-abd4-463e5022bdc3")]
+
+// Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      Alt Sürüm
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya Gözden Geçirme ve Yapı Numaralarını varsayılan değerlerine döndürebilirsiniz
+// gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
