a24eeb130b5e627a862415e3cbb6f906e81726d2
IcedFire34 <omar.faruk.bozkaya@gmail.com>
2024-07-31 16:17:01 +0300

    Controllerlar tekrar düzenlendi. Otoparklar sayfası ve 2 tane otopark eklendi

---
+++ pentas/pentas/Controllers/OtoparklarController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pentas.Controllers
+{
+    public class OtoparklarController : Controller
+    {
+        // GET: Otoparklar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Havaalani_Otoparki()
+        {
+            return View();
+        }
+        public ActionResult Merkez_Sahil_Otoparki()
+        {
+            return View();
+        }
+    }
+}
---
+++ pentas/pentas/Controllers/TesislerController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pentas.Controllers
+{
+    public class TesislerController : Controller
+    {
+        // GET: Tesisler
+        public ActionResult Guzelyali_Sosyal_Tesisleri()
+        {
+            return View();
+        }
+        public ActionResult PENCR_Bahce_Kafe()
+        {
+            return View();
+        }
+        public ActionResult PENCR_Burun()
+        {
+            return View();
+        }
+        public ActionResult PENCR_Carsi()
+        {
+            return View();
+        }
+        public ActionResult PENCR_Sahil()
+        {
+            return View();
+        }
+        public ActionResult PENCR_Koru()
+        {
+            return View();
+        }
+        public ActionResult PENCR_Kurtkoy()
+        {
+            return View();
+        }
+        public ActionResult Durak_Büfe()
+        {
+            return View();
+        }
+        public ActionResult Cay_Ocaklari()
+        {
+            return View();
+        }
+        public ActionResult Yenisehir_Piknik_Alani()
+        {
+            return View();
+        }
+        public ActionResult Kunrakoy_Piknik_Alani()
+        {
+            return View();
+        }
+    }
+}
