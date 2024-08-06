ee230a4a961cddbb979043f313e96da4c7b58bc2
IcedFire34 <omar.faruk.bozkaya@gmail.com>
2024-08-06 16:53:32 +0300

    Tesisler sayfasındaki tabloya veriler veri tabanından geliyor. tesisler kısmındaki veriler tamamen eklendi.

--- pentas/pentas/Controllers/AccountController.cs
+++ pentas/pentas/Controllers/AccountController.cs
@@ -1,63 +1,55 @@
-﻿using pentas.Models;
-using System;
-using System.Collections.Generic;
+﻿using pentas.Data;
+using pentas.Models;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Web.Security;
-using System.Data.SqlClient;
 
 namespace pentas.Controllers
 {
     public class AccountController : Controller
     {
-        SqlConnection _connection = new SqlConnection();
-        SqlCommand _command = new SqlCommand();        
+        private readonly ApplicationDbContext _context;
+
+        public AccountController()
+        {
+            _context = new ApplicationDbContext();
+        }
 
         [HttpGet]
         public ActionResult Login()
         {
             return View();
         }
-        public ActionResult Index()
-        {
-            return View();
-        }
-        public ActionResult Tesisler()
-        {
-            return View();
-        }
-
-        void connectionString() 
-        {
-            _connection.ConnectionString = "data source=DESKTOP-DFQMG2T; database=pentasDB; integrated security = SSPI";            
-        }
 
         [HttpPost]
         public ActionResult Verify(User user)
         {
-            connectionString();
-            using (var connection = new SqlConnection(_connection.ConnectionString))
+            if (ModelState.IsValid)
             {
-                connection.Open();
-                using (var command = new SqlCommand("SELECT * FROM [User] WHERE email = @Email AND paswd = @Password", connection))
+                var validUser = _context.Users
+                    .Where(u => u.email == user.email && u.paswd == user.paswd)
+                    .FirstOrDefault();
+
+                if (validUser != null)
                 {
-                    command.Parameters.AddWithValue("@Email", user.email);
-                    command.Parameters.AddWithValue("@Password", user.paswd);
-                    using (var reader = command.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            return View("Index");
-                        }
-                        else
-                        {
-                            return View("Login");
-                        }
-                    }
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Geçersiz giriş");
                 }
             }
+            return View("Login");
         }
 
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Tesisler()
+        {
+            var tesisler = _context.Tesisler.ToList();
+            return View(tesisler);
+        }
     }
 }
